6629f9ba0aa75448abbdd4f840ebb2a2d0fcefbb
Archit Sharma <archit@pm.me>
2019-09-09 17:06:39 +0530

    OP-635: attempt to make markdown checks build-fail friendly

--- ci/markdown-link-check.sh
+++ ci/markdown-link-check.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+command -v remark >/dev/null 2>&1 || { echo >&2 "Package 'remark' not installed.  Aborting."; exit 1; }
+
 LINT_OUTPUT=`remark -u validate-links -u lint-no-dead-urls . 2>&1`
 
 LINT_OUTPUT_MODIFIED=`printf '%s\n' "${LINT_OUTPUT[@]}" | grep -v 'localhost\|warnings'`
 
 if echo $LINT_OUTPUT_MODIFIED | grep -i "warning"; then
-    printf '%s\n' "${LINT_OUTPUT[@]}"
+    printf '%s\n' "${LINT_OUTPUT_MODIFIED[@]}"
     echo ""
     echo "Please Fix the above broken links! Please ignore any localhost warnings."
     exit 1
